2aaa48a25d421f295ebc1f700862e16423ccd10f
dylan guerrero <dylanjose619@udenar.edu.co>
2024-06-02 11:56:43 -0500

    corrigiento tipos de bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { createApp } from 'vue'
+import { createApp } from "vue";
+//@ts-ignore
 import bootstrap from "bootstrap/dist/js/bootstrap.bundle";
-import './style.css'
-import App from './App.vue'
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
---
+++ src/types/bootstrap.d.ts
@@ -0,0 +1,4 @@
+declare module "bootstrap/dist/js/bootstrap.bundle" {
+  const bootstrap: any;
+  export default bootstrap;
+}
